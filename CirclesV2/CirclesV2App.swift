a29b1551676b9da747af34a1ec37c6a3ad66cd91
Varun Nair <vnairr314@gmail.com>
2023-08-11 10:45:59 -0400

    Initial Commit

---
+++ CirclesV2/CirclesV2App.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesV2App.swift
+//  CirclesV2
+//
+//  Created by Varun Nair on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CirclesV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CirclesV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CirclesV2
+//
+//  Created by Varun Nair on 8/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
